c451f0553249826f7246655574782a1d78b2c500
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-11-02 16:07:34 +0100

    lecturer table added

--- database/migrations/2021_10_23_054712_create_admins_table.php
+++ database/migrations/2021_10_23_054712_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLecturersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateLecturersTable extends Migration
      */
     public function up()
     {
-        Schema::create('lecturers', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('phone_number');
-            $table->string('faculty')->nullable();
-            $table->string('department')->nullable();
-            $table->string('qualification')->nullable();
-            $table->string('gender')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -35,6 +31,6 @@ class CreateLecturersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lecturers');
+        Schema::dropIfExists('admins');
     }
 }
