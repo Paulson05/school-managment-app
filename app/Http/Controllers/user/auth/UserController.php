54ad6c4fab372c5391c91faf2d9f30e60c902cf9
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-12-23 19:57:23 +0100

    user controller'

--- app/Http/Controllers/user/auth/UserController.php
+++ app/Http/Controllers/user/auth/UserController.php
@@ -11,7 +11,7 @@ use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class userController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Exam\QuizController;
 use App\Http\Controllers\Lecturer\LecturerController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\RolesController;
+use App\Http\Controllers\user\auth\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
